38edd1c0e85252774eb5267cb8902437957e6fee
final898y <final898y@gmail.com>
2025-05-19 21:30:12 +0800

    fix. vite設定https只在開發中開啟。

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,21 @@ import tailwindcss from "@tailwindcss/vite";
 import fs from "fs";
 import path from "path";
 
-// https://vite.dev/config/
+const isLocal = process.env.NODE_ENV !== "production";
+
 export default defineConfig({
   plugins: [tailwindcss(), vue()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "cert.crt")),
-    },
-    port: 5173,
-  },
+  server: isLocal
+    ? {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, "key.pem")),
+          cert: fs.readFileSync(path.resolve(__dirname, "cert.crt")),
+        },
+        port: 5173,
+      }
+    : {
+        port: 5173,
+      },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
